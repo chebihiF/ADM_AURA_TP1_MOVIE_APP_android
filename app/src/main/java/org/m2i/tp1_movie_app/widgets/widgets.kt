9aa34f42588006851e420b7f00d4bd53b9b2b907
Fayçal Chebihi <chebihifaycal.it@gmail.com>
2023-09-06 15:31:24 +0100

    finishing design HomeScreen

--- app/src/main/java/org/m2i/tp1_movie_app/widgets/widgets.kt
+++ app/src/main/java/org/m2i/tp1_movie_app/widgets/widgets.kt
@@ -1,7 +1,10 @@
 package org.m2i.tp1_movie_app.widgets
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -11,28 +14,53 @@ import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import coil.compose.AsyncImagePainter
+import coil.compose.rememberAsyncImagePainter
+import coil.compose.rememberImagePainter
+import coil.transform.CircleCropTransformation
 import org.m2i.tp1_movie_app.model.Movie
 import org.m2i.tp1_movie_app.model.getMovies
 
 @Preview
 @Composable
 fun MovieRow(movie: Movie = getMovies()[0], onItemClick: (String) -> Unit = {}) {
+
+    var expanded by remember {
+        mutableStateOf(false)
+    }
+
     Card(modifier = Modifier
         .padding(4.dp)
         .fillMaxWidth()
-        .height(130.dp)
+
         .clickable {
             onItemClick(movie.id)
         },
@@ -49,10 +77,65 @@ fun MovieRow(movie: Movie = getMovies()[0], onItemClick: (String) -> Unit = {}) 
                 shape = RectangleShape,
                 shadowElevation = 4.dp,
             ) {
-                Icon(imageVector = Icons.Default.AccountBox,contentDescription = "Movie Image")
+
+                AsyncImage(
+                    model = movie.images[0],
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                )
+
+                /*Image(painter = rememberImagePainter(data = movie.images[0]),
+                    contentDescription = "Poster")*/
             }
 
-            Text(text = movie.title)
+            Column(modifier = Modifier.padding(4.dp)) {
+                Text(text = movie.title,
+                    style = MaterialTheme.typography.headlineSmall)
+                Text(text = "Director : ${movie.director}",
+                    style = MaterialTheme.typography.labelMedium)
+                Text(text = "Released : ${movie.year}",
+                    style = MaterialTheme.typography.labelMedium)
+
+                AnimatedVisibility(visible = expanded) {
+                    Column {
+                        /*Text(text = "Actors : ${movie.actors}",
+                            style = MaterialTheme.typography.labelMedium)
+                        Text(buildAnnotatedString {
+                            withStyle(style = SpanStyle(color = Color.DarkGray,
+                                fontSize = 13.sp, fontWeight = FontWeight.Bold
+                            )){
+                                append("Plot : ")
+                            }
+                            withStyle(style = SpanStyle(color = Color.DarkGray,
+                                fontSize = 13.sp
+                            )){
+                                append(movie.plot)
+                            }
+                        }, modifier = Modifier.padding(6.dp))*/
+                        Divider()
+                        Text(text = "Actors : ${movie.actors}",
+                            style = MaterialTheme.typography.labelMedium)
+                        Text(text = "Genre : ${movie.genre}",
+                            style = MaterialTheme.typography.labelMedium)
+                        Text(text = "Rating : ${movie.rating}",
+                            style = MaterialTheme.typography.labelMedium)
+                    }
+
+
+
+                }
+
+
+                Icon(imageVector =
+                if(!expanded) Icons.Filled.KeyboardArrowDown
+                else Icons.Filled.KeyboardArrowUp ,
+                    contentDescription = "details",
+                    modifier = Modifier
+                        .size(25.dp)
+                        .clickable { expanded = !expanded },
+                    tint = Color.DarkGray)
+            }
         }
+
     }
 }
